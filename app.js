25b0a34dc4bd13fe4ef975bccf2ffd990a38297f
brianherbas <brianherbas.412@gmail.com>
2022-09-26 14:19:44 -0300

    server is up and running

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Store API');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('store api');
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server is listening on port ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
