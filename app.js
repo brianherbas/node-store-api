60332e1b741da6a9fb699ae2db18edd0476c81b2
brianherbas <brianherbas.412@gmail.com>
2022-09-26 14:24:32 -0300

    middlewares - basic custom errors

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+// app.use(express.json())
+
+// routes
+
 app.get('/', (req, res) => {
-  res.send('store api');
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
